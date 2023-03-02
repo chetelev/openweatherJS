52e3836038df69bca7e9337eb5e36bfe62f21f15
chetelev <chetelev@gmail.com>
2023-03-02 10:08:13 +0100

    update api key

--- js/weather.js
+++ js/weather.js
@@ -1,12 +1,12 @@
 class Weather {
     constructor() {
-        this.key = 'bfac2187b0c99dfe4c3d79e029a91092'
+        this.key = 'b1cbd2549e4f7f5e852719b6c3e27b81'
         this.celsius = 'metric'
     }
 
     // Fetching API for multi cities
     async getWeather(metric) {
-        const multipleCities = await fetch(`http://api.openweathermap.org/data/2.5/group?id=524901,792680,703448,2643743&units=${metric === undefined ? this.celsius : metric === null ? '' : metric}&appid=${this.key}`)
+        const multipleCities = await fetch(`http://api.openweathermap.org/data/2.5/group?id=524901,792680,703448,2643743&units=${metric === undefined ? this.celsius : metric === null ? '' : metric}&APPID=${this.key}`)
         const multiCity = await multipleCities.json()
         return {
             multiCity
@@ -15,7 +15,7 @@ class Weather {
 
     // Fetching API for single cities    
     async getSingleWeather(searchCity, metric) {
-        const targetCity = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${searchCity}&units=${metric === undefined || metric === 'activeCelsius' ? this.celsius : metric}&appid=${this.key}`)
+        const targetCity = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${searchCity}&units=${metric === undefined || metric === 'activeCelsius' ? this.celsius : metric}&APPID=${this.key}`)
         const city = await targetCity.json()
         return {
             city
